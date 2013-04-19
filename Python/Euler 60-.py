85331b809600b245f5fce4cb32ff3514a4738ad5
Jackie Baek <jackiewbaek@gmail.com>
2013-04-18 21:57:16 -0400

    problem 81

--- Python/Euler 60-.py
+++ Python/Euler 60-.py
@@ -291,6 +291,47 @@ def prob71():
     return left_num, left_denom
 
 """
+PROBLEM 81
+
+In the 5 by 5 matrix below, the minimal path sum from the top left to the bottom right,
+by only moving to the right and down, is indicated in bold red and is equal to 2427.
+
+
+131	673	234	103	18
+201	96	342	965	150
+630	803	746	422	111
+537	699	497	121	956
+805	732	524	37	331
+
+Find the minimal path sum, in matrix.txt (right click and 'Save Link/Target As...'),
+a 31K text file containing a 80 by 80 matrix, from the top left to the bottom right
+by only moving right and down.
+"""
+prob81max = 79
+def prob81():
+    f = open("prob81.txt")
+    matrix= []
+    for line in f:
+        matrix.append([int(l) for l in line.split(",")])
+    print matrix
+    memoized = [[-1 for i in range(80)] for j in range(80)]
+    return findMinPath81(0,0,matrix, memoized)
+
+def findMinPath81(row, col, matrix, memoized):
+    curr = matrix[row][col]
+    if row == prob81max and col == prob81max:
+        print 'MAX', curr
+        return curr
+    elif memoized[row][col] == -1:
+        if row == prob81max:
+            memoized[row][col] = curr+findMinPath81(row, col+1, matrix, memoized)
+        elif col == prob81max:
+            memoized[row][col] = curr+findMinPath81(row+1, col, matrix, memoized)
+        else:
+            memoized[row][col] = curr+min(findMinPath81(row+1, col, matrix, memoized),findMinPath(row, col+1, matrix, memoized))
+    return memoized[row][col]
+
+"""
 PROBLEM 97
 The first known prime found to exceed one million digits was discovered in 1999,
 and is a Mersenne prime of the form 269725931; it contains exactly 2,098,960 digits.
